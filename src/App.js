ea2382899c260cfd20bffd30bfde922ebbe76191
yetifunk <charlesdemar87@gmail.com>
2021-06-25 09:53:00 -0600

    to do list functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import './App.css';
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import Header from './components/Header/Header'
 import WeatherSearch from './components/WeatherSearch/WeatherSearch';
 import CoinSearch from './components/CoinSearch/CoinSearch'
 import ToDoList from './components/ToDoList/ToDoList'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-
       <Header />
-
-      <Route 
-        path="/"
-        render={() => <WeatherSearch />}
-      />
-
-      <Route
-        path="/"
-        render={() => <CoinSearch />}
-      />
-
-      <Route
-        path="/"
-        render={() => <ToDoList />}
-      />
-
+      <Route path="/" render={() => <WeatherSearch />} />
+      <Route path="/" render={() => <CoinSearch />} />
+      <Route path="/" render={() => <ToDoList />} />
     </div>
   );
 }
-
-export default App;
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,5 +1,49 @@
+import { useState } from 'react'
+import ToDoForm from './ToDoForm/ToDoForm'
+import ToDoData from './ToDoForm/ToDoData/ToDoData'
+import ToDoItem from './ToDoForm/ToDoData/ToDoItem/ToDoItem';
+
 export default function ToDoList() {
+
+    const [todos, setToDos] = useState([]);
+    const initialFormState = { title: '', completed: false };
+    const [newToDo, setNewToDo] = useState(initialFormState);
+
+    function handleChange(event) {
+        setNewToDo({...newToDo, [event.target.id]: event.target.value })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setToDos([...todos, newToDo]);
+        setNewToDo(initialFormState);
+    }
+
+    function updateToDo(title) {
+        const tempList = [...todos];
+        const idx = tempList.findIndex((item) => item.title === title)
+        const tempToDo = tempList.splice(idx, 1)[0]
+        tempToDo.completed = true;
+        setToDos([...tempList, tempToDo])
+    }
+
+    function removeToDo(title) {
+        const tempList = [...todos]
+        const idx = tempList.findIndex((item) => item.title === title)
+        tempList.splice(idx, 1)
+        setToDos(tempList)
+    }
     return (
-        <h2>Hello from ToDoList</h2>
+        <div className="to-do-container">
+            <h2>Hello from ToDoList</h2>
+            <ToDoForm handleChange={handleChange} handleSubmit={handleSubmit} updateToDo={updateToDo} removeToDo={removeToDo} newToDo={newToDo} />
+            <div className="list-wrapper">
+                <h2>To Do Items:</h2>
+                    <ToDoData todos={todos.filter((item) => !item.completed)} clickHandler={updateToDo} />
+                    <h2>Completed Items:</h2>
+                    <ToDoData todos={todos.filter((item) => item.completed)} clickHandler={removeToDo} />
+            </div>
+        </div>
+
     )
 }
