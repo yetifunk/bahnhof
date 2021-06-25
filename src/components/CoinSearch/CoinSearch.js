5cbd9b7c066d371a3dd7dca892af30ca37540428
yetifunk <charlesdemar87@gmail.com>
2021-06-25 07:59:58 -0600

    coin functionality

--- src/components/CoinSearch/CoinSearch.js
+++ src/components/CoinSearch/CoinSearch.js
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+import CoinSearchResults from "./CoinSearchResults/CoinSearchResults";
+
 export default function CoinSearch() {
+    ////Coin Handelers
+    const [coinPrice, setCoinPrice] = useState([]);
+
+    useEffect(() => {
+        getCoinPrices();
+    }, []);
+
+    async function getCoinPrices() {
+        const COIN_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Cethereum%2Cmonero&vs_currencies=usd&include_24hr_change=true"
+
+        await fetch(COIN_URL)
+        .then(res => res.json())
+        .then(result => {
+            setCoinPrice(result)
+        })
+        .catch(console.error);
+    }
+
     return (
-        <h2>Hello From CoinSearch</h2>
+        <div className="coin-container">
+            <div className="coin-header">
+                <h2>Hello From CoinSearch</h2>
+            </div>
+            
+            <div className="coin-results">
+                <CoinSearchResults coinPrice={coinPrice} />
+            </div>
+        </div>
     )
 }
